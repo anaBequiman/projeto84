d21c6091e09bb5b8c45d2c17effd31fc93ef40a6
Ana <101293323+anaBequiman@users.noreply.github.com>
2022-06-27 10:47:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+canvas = document.getElementById("meuCanvas");
+ctx = canvas.getContext("2d");
+larguraImg = 300;
+alturaImg = 100;
+var imagem;
+imgX = 100;
+imgY = 100;
+function add() {
+	img_imgTag = new Image();
+	img_imgTag.onload = uploadimg;
+	img_imgTag.src = imagem;
+}
+function uploadimg() {
+	ctx.drawImage(img_imgTag, imgX, imgY, larguraImg, alturaImg);
+}
+window.addEventListener("keydown", my_keydown)
+function my_keydown(e){
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	//teclas do alfabeto
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+			aplhabetkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma tecla do alfabeto";
+			console.log("Tecla do alfabeto")
+		}
+	//tecla dos números
+		if((keyPressed >=48 && keyPressed <=57)){
+			numberkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma tecla dos números";
+			console.log("Tecla numérica")
+		}
+	//tecla direcional
+		if((keyPressed >=37 && keyPressed <=40)){
+			arrowkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma tecla direcional";
+			console.log("Tecla direcional")
+		}
+	//tecla especial
+		if((keyPressed == 17 && 18)){
+			specialkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma especial";
+			console.log("Tecla especial")
+		}
+	//outra tecla
+		else {
+			otherkey();
+			document.getElementById("d1").innerHTML="Você pressionou o símbolo ou outra tecla";
+		}
+}
+function aplhabetkey(){
+	imagem = "TECLA ALFABÉTICA.png";
+	add();
+}
+function numberkey(){
+	imagem = "TECLA NUMÉRICA.png";
+	add();
+}
+function arrowkey(){
+	imagem = "TECLA DIRECIONAL.png";
+	add();
+}
+function specialkey(){	
+	imagem = "TECLA ESPECIAL.png";
+	add();
+}
+function otherkey(){
+	imagem="OUTRA TECLA.png";
+	add();
+}
